09aada7ea29d19c69e513b169c996f61763a0772
Marina-Banov <marina.banov@uniri.hr>
2026-03-02 22:02:18 +0100

    move unpublished docs to new folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Marina-Banov.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/os1-docs/',
@@ -29,6 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Marina-Banov', // Usually your GitHub org/user name.
   projectName: 'os1-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
